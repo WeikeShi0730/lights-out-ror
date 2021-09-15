7ad124d2f95ff367f10ee4c8f1a679f22c0f120d
Weike Shi <weike.shi@ericsson.com>
2021-09-14 22:06:25 -0400

    added a header

---
+++ app/javascript/components/header/header.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1 className="bg-red-300">header</h1>;
+};
+
+export default Header;
--- app/javascript/pages/main.pages.jsx
+++ app/javascript/pages/main.pages.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./main.styles.scss";
 
 import Lights from "../components/lights/lights.component";
+import Header from "../components/header/header.component";
 
 const Main = ({ clickedInside }) => {
   return (
-    <div className="main">
-      <Lights clickedInside={clickedInside} />
-    </div>
+    <React.Fragment>
+      <Header />
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-2 main">
+          <Lights clickedInside={clickedInside} />
+        </div>
+        <h1 className="flex justify-center">leader board</h1>
+      </div>
+    </React.Fragment>
   );
 };
 
